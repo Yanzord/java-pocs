200b6ec7984e03ced4a2e7367f449cda573640b7
yanzord <yan.pereira@ilegra.com>
2020-01-21 15:04:29 -0300

    Update infra Jenkinsfile, refactor tests

--- java/springboot-rest-application/src/test/java/com/yanzord/cloud/springbootrestapplication/service/HelloServiceTest.java
+++ java/springboot-rest-application/src/test/java/com/yanzord/cloud/springbootrestapplication/service/HelloServiceTest.java
@@ -1,12 +1,16 @@
 package com.yanzord.cloud.springbootrestapplication.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest(classes={HelloService.class})
 public class HelloServiceTest {
 
-    private HelloService helloService = new HelloService();
+    @Autowired
+    private HelloService helloService;
 
     @Test
     public void shouldGetHello() {
