27385413fcc41cd003ae7e3f5054215a0632eeb4
yanzord <yan.pereira@ilegra.com>
2020-01-22 15:35:55 -0300

    Update ansible install script

--- java/springboot-rest/infra/ansible/ansible.sh
+++ java/springboot-rest/infra/ansible/ansible.sh
@@ -3,3 +3,8 @@
 # Install Ansible repository
 apt -y update && apt-get -y upgrade
 apt -y install software-properties-common
+apt-add-repository ppa:ansible/ansible
+
+#Install Ansible
+apt -y update
+apt -y install ansible
