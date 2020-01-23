1cefbfb36c43b945543ecc8a0d186e26e03c2c89
yanzord <yan.pereira@ilegra.com>
2020-01-23 14:56:15 -0300

    Update ansible_python.sh script

--- java/springboot-rest/infra/ansible_python.sh
+++ java/springboot-rest/infra/ansible_python.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-# Install Ansible and python repository
-apt -y update && apt-get -y upgrade
-apt -y install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-add-repository ppa:jonathonf/python-3.6
-
-#Install Ansible and python
 apt -y update
-apt -y install ansible
-apt -y install python3.6
+apt-get -y install ansible
+apt-get -y install python3 python3-apt -yq
